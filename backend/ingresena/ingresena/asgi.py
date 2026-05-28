500889390eaeab2542e6bd72fd334956c78ffbc8
keiner <bastidasnkeiner21@gmail.com>
2026-05-28 11:47:51 -0500

    se sube estructura inicial del proyecto

---
+++ backend/ingresena/ingresena/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ingresena project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ingresena.settings')
+
+application = get_asgi_application()
